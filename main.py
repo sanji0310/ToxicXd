259d0b5815d44642d3b60575648b1d01aa16aedb
sanji0310 <cardbygloosmoke@gmail.com>
2024-06-19 21:02:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -133,6 +133,7 @@ def main(message):
 					bot.reply_to(message, msg)
 				else:
 					dd += 1
+					time.sleep(1)
 	except Exception as e:
 		print(e)
 	bot.edit_message_text(chat_id=message.chat.id, message_id=ko, text='𝗕𝗘𝗘𝗡 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗘𝗗 ✅\n𝗕𝗢𝗧 𝗕𝗬 ➜ @strawhatchannel69')
